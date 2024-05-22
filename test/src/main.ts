08f338a5237c1e8ada1c24997b8907678662d026
gabrielcassolbach <gabrielcassolbach@gmail.com>
2024-05-22 10:37:38 -0300

    fix: security problems solved

--- test/src/main.ts
+++ test/src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dynamoose from 'dynamoose';
-import process = require('process');
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Create new DynamoDB instance
+
   const ddb = new dynamoose.aws.ddb.DynamoDB({
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.secretAccessKey,
+      accessKeyId: process.env.DB_ACCESS_KEY,
+      secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
     },
     region: 'us-east-2',
   });
-
-  //console.log(process.env.AWS_ACCESS_KEY_ID);
 
   // Set DynamoDB instance to the Dynamoose DDB instance
   dynamoose.aws.ddb.set(ddb);
